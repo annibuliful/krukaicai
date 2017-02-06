4de2bfb25cb1b273029464912a795850d6b8e16f
jarupong pajakgo <lagmanmc@gmail.com>
2017-02-06 12:03:35 +0700

    register function tested

--- backend/model/DB/authen/authen.php
+++ backend/model/DB/authen/authen.php
@@ -1,5 +1,6 @@
 <?php
 
+session_start();
 include dirname(__DIR__).'\..\..\config\DB.php';
 class authen
 {
@@ -11,12 +12,12 @@ class authen
         $this->sql = new PDO($pdo[0], $pdo[1], $pdo[2]);
         $this->sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
-    public function register(string $user, string $password, string $email, int $flag, string $name, string $surname, array $image, string $payment_number = null)
+    public function register(string $user, string $password, string $email, string $name)
     {
         try {
-            if ($user != null && $password != null && $email != null && $flag != null) {
+            if ($user != null && $password != null && $email != null) {
                 $hash_pass = password_hash($password, PASSWORD_DEFAULT);
-                $sql = $this->sql->prepare('SELECT * FROM user WHERE username = :user OR email= :email ;');
+                $sql = $this->sql->prepare('SELECT * FROM user WHERE user = :user OR email= :email ;');
                 $sql->bindParam(':user', $user);
                 $sql->bindParam(':email', $email);
                 $sql->execute();
@@ -24,20 +25,13 @@ class authen
                 if ($fetch) {
                     return 'have_user';
                 } else {
-                    $sql = $this->sql->prepare('INSERT INTO user(username,name,surname,
-                                          password,image,email,score,money,rating,payment_number)
-                                          VALUES (:user ,:name ,:surname ,:password ,:image ,:email ,
-                                          0,100,0,:payment_number );');
+                    $sql = $this->sql->prepare('INSERT INTO user(user,name,password,email,score)
+                                                VALUES (:user ,:name ,:password ,:email , 0 );');
                     $sql->bindParam(':user', $user, PDO::PARAM_STR);
                     $sql->bindParam(':password', $hash_pass, PDO::PARAM_STR);
                     $sql->bindParam(':email', $email, PDO::PARAM_STR);
                     $sql->bindParam(':name', $name, PDO::PARAM_STR);
-                    $sql->bindParam(':surname', $surname, PDO::PARAM_STR);
-                    $sql->bindParam(':image', $image['name'], PDO::PARAM_STR);
-                    $sql->bindParam(':payment_number', $payment_number, PDO::PARAM_STR);
                     $sql->execute();
-                    move_uploaded_file($image['tmp_name'],
-                               '../../frontend/store/pictures/'.$image['name']);
 
                     return true;
                 }
@@ -87,5 +81,6 @@ class authen
             return false;
         }
     }
+
 }
 ?>
