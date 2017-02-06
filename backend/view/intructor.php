76b8f0e9a84d226b05c1955b1c746f2f546f4d52
jarupong pajakgo <lagmanmc@gmail.com>
2017-02-06 22:21:13 +0700

    file for intructor view

---
+++ backend/view/intructor.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class intructor_view
+{
+    public function __construct()
+    {
+    }
+
+    public function contentComplete()
+    {
+        echo 'สร้างเนื้อหาสำเร็จ';
+    }
+
+    public function contentFail()
+    {
+        echo 'เกิดปัญหาในการสร้าง';
+    }
+}
